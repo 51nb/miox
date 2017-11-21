24c7c7b8f9db0b881f3f6d44c3e6dd20e2f2d9e2
shenyunjie <shenyunjie@u51.com>
2017-11-21 16:41:42 +0800

    add cmd/git.js and cmd/lazy.js.

--- cmd/lazy.js
+++ cmd/lazy.js
@@ -5,7 +5,7 @@ const msg = argv[2];
 
 (async () => {
   await compile(
-    `npm run version && npm run noistanbul && npm run build && npm run hasistanbul && npm run git ${msg}`, 
+    `npm run version && npm run noistanbul && npm run build && npm run hasistanbul && npm run git "${msg}"`, 
     path.resolve(__dirname, '..')
   );
   process.exit(0);
